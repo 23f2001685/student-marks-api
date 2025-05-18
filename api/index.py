b8cb81d00f3c890c47bc7bc2e5f480823bc9ea74
23f2001685 <23f2001685@ds.study.iitm.ac.in>
2025-05-18 15:04:03 +0530

    Update response

--- api/index.py
+++ api/index.py
@@ -27,7 +27,7 @@ async def get_marks(name: List[str] = Query(None)):
         if i["name"] in name:
             marks.append(i["marks"])
 
-    return json({ "marks": marks })
+    return json.dumps({ "marks": marks })
 
 @app.get("/")
 async def root():
